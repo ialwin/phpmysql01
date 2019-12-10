91cc784330886cece42e74037e0b1c81cc15072b
ialwin <57170314+ialwin@users.noreply.github.com>
2019-12-10 12:12:07 +0700

    Add files via upload

---
+++ dbprint.php
@@ -0,0 +1,37 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("databaseuser");
+$dbpwd = getenv("databasepassword");
+$dbname = getenv("databasename");
+
+$con=mysqli_connect($dbhost,$dbuser,$dbpwd,$dbname);
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM ossuser");
+
+echo "<table border='1'>
+<tr>
+<th>ID</th>
+<th>First_Name</th>
+<th>Last_Name</th>
+<th>Email</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['id'] . "</td>";
+echo "<td>" . $row['first_name'] . "</td>";
+echo "<td>" . $row['last_name'] . "</td>";
+echo "<td>" . $row['email'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
